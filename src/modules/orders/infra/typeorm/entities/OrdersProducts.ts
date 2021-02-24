6cc204295909d2515c12c3b9f4da0718f652f062
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2021-02-24 20:16:15 -0300

    finished challenge

--- src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/infra/typeorm/entities/OrdersProducts.ts
@@ -10,24 +10,41 @@ import {
 
 import Order from '@modules/orders/infra/typeorm/entities/Order';
 import Product from '@modules/products/infra/typeorm/entities/Product';
+import ColumnNumericTransformer from '@shared/utils/ColumnNumericTransformer';
 
+@Entity('orders_products')
 class OrdersProducts {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  order: Order;
-
-  product: Product;
-
-  product_id: string;
-
+  @Column()
   order_id: string;
 
+  @ManyToOne(() => Order)
+  @JoinColumn({ name: 'order_id' })
+  order: Order;
+
+  @Column()
+  product_id: string;
+
+  @ManyToOne(() => Product, { eager: true })
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column('decimal')
   price: number;
 
+  @Column('decimal', {
+    precision: 10,
+    scale: 2,
+    transformer: new ColumnNumericTransformer(),
+  })
   quantity: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -8,20 +8,42 @@ import {
 } from 'typeorm';
 
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
+import ColumnNumericTransformer from '@shared/utils/ColumnNumericTransformer';
 
+@Entity('products')
 class Product {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   name: string;
 
+  @Column('decimal', {
+    precision: 10,
+    scale: 2,
+  })
   price: number;
 
+  @Column('decimal', {
+    precision: 10,
+    scale: 2,
+    transformer: new ColumnNumericTransformer(),
+  })
   quantity: number;
 
+  @OneToMany(
+    () => OrdersProducts,
+    ordersProducts => ordersProducts.product_id,
+    {
+      cascade: true,
+    },
+  )
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
